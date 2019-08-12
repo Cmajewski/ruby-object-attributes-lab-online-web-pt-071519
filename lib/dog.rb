2ce7477d93ae6c0d318c81283a559b64625b8d5b
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-12 03:33:20 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(dog_name)
+    @doggos_name=dog_name
+  end
+  def name
+    @doggos_name
+  end
+  def breed=(dog_breed)
+    @doggo_breed=dog_breed
+  end
+  def breed
+    @doggo_breed
+  end
+end 
